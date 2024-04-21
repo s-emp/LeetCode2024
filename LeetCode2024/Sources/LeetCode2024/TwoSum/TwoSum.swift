db19fee25dc6d16909d2771ccf78d1d5b057560b
Sergey Melnikov <smelnikov2@alfabank.ru>
2024-04-22 01:04:53 +0800

    112: TwoSum - solution

--- LeetCode2024/Sources/LeetCode2024/TwoSum/TwoSum.swift
+++ LeetCode2024/Sources/LeetCode2024/TwoSum/TwoSum.swift
@@ -1,5 +1,14 @@
 class TwoSum {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var dictionary = Dictionary<Int, Int>()
+        for i in 0..<nums.count {
+            let value = nums[i]
+            if let j = dictionary[target - value] {
+                return [j, i]
+            } else {
+                dictionary[value] = i
+            }
+        }
         return []
     }
 }
