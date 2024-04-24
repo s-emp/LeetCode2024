b4d0111c148c625047c9ef6463f597c33bdfec80
Sergey Melnikov <smelnikov2@alfabank.ru>
2024-04-24 12:03:53 +0800

    114: ValidWorldSquare - solution

--- LeetCode2024/Sources/LeetCode2024/ValidWorldSquare/ValidWorldSquare.swift
+++ LeetCode2024/Sources/LeetCode2024/ValidWorldSquare/ValidWorldSquare.swift
@@ -2,6 +2,14 @@
 
 final class ValidWorldSquare {
     func validWordSquare(_ words: [String]) -> Bool {
-        false
+        let words = words.map { Array($0) }
+        for i in 0..<words.count {
+            for j in 0..<words[i].count {
+                if j >= words.count || i >= words[j].count || words[i][j] != words[j][i] {
+                    return false
+                }
+            }
+        }
+        return true
     }
 }
