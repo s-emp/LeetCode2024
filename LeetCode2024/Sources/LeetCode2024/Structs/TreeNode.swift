6a968e4e5e013a391172a3f3e65cf3bc4c9b48b5
Sergey Melnikov <smelnikov2@alfabank.ru>
2024-04-25 14:43:00 +0800

    115: Binary Tree Vertical Order Traversal - not a working solution

--- LeetCode2024/Sources/LeetCode2024/Structs/TreeNode.swift
+++ LeetCode2024/Sources/LeetCode2024/Structs/TreeNode.swift
@@ -10,3 +10,35 @@ public class TreeNode {
         self.right = right
     }
 }
+
+
+public class TwoList {
+    public var val: [Int]
+    public var left: TwoList?
+    public var right: TwoList?
+    
+    public init(val: [Int], left: TwoList? = nil, right: TwoList? = nil) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+    
+    func getArray() -> [[Int]] { // что бы собрать я опять прохожусь по всему массиву, что не очень быстро
+        var leftArray = [[Int]]()
+        var currentLeft = left
+        while currentLeft != nil {
+            leftArray.append(currentLeft!.val)
+            currentLeft = currentLeft!.left
+        }
+        var rightArray = [[Int]]()
+        var currentRight = right
+        while currentRight != nil {
+            rightArray.append(currentRight!.val)
+            currentRight = currentRight!.right
+        }
+        leftArray.reverse() // медленно
+        leftArray.append(val)
+        leftArray.append(contentsOf: rightArray)
+        return leftArray
+    }
+}
--- LeetCode2024/Sources/LeetCode2024/VerticalOrderTraversal/VerticalOrderTraversal.swift
+++ LeetCode2024/Sources/LeetCode2024/VerticalOrderTraversal/VerticalOrderTraversal.swift
@@ -2,6 +2,32 @@
 
 final class VerticalOrderTraversal {
     func verticalOrder(_ root: TreeNode?) -> [[Int]] {
-        [[]]
+        guard let root else { return [[]] }
+        let twoList = TwoList(val: [])
+        parse(node: root, list: twoList)
+        var columns: [Int: [Int]] = [:]
+        columns.keys.min()
+        return twoList.getArray()
+        
+    }
+    
+    fileprivate func parse(node: TreeNode, list: TwoList) {
+        list.val.append(node.val)
+        if let leftNode = node.left {
+            if let leftList = list.left {
+                parse(node: leftNode, list: leftList)
+            } else {
+                list.left = .init(val: [], left: nil, right: list)
+                parse(node: leftNode, list: list.left!)
+            }
+        }
+        if let rightNode = node.right {
+            if let rightList = list.right {
+                parse(node: rightNode, list: rightList)
+            } else {
+                list.right = .init(val: [], left: list, right: nil)
+                parse(node: rightNode, list: list.right!)
+            }
+        }
     }
 }
