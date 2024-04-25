971d5e8e6fc8a485d6c6b5087c69ea4acc80d219
Sergey Melnikov <smelnikov2@alfabank.ru>
2024-04-25 13:32:27 +0800

    115: Binary Tree Vertical Order Traversal - tests

---
+++ LeetCode2024/Sources/LeetCode2024/VerticalOrderTraversal/VerticalOrderTraversal.swift
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/binary-tree-vertical-order-traversal/description/
+
+final class VerticalOrderTraversal {
+    func verticalOrder(_ root: TreeNode?) -> [[Int]] {
+        [[]]
+    }
+}
---
+++ LeetCode2024/Tests/LeetCode2024Tests/VerticalOrderTraversal/VerticalOrderTraversalTests.swift
@@ -0,0 +1,44 @@
+import XCTest
+@testable import LeetCode2024
+
+final class VerticalOrderTraversalTests: XCTestCase {
+    var solution: VerticalOrderTraversal!
+    
+    override func setUp() {
+        solution = .init()
+    }
+    
+    func testVerticalOrderTraversal() {
+        XCTAssertEqual(
+            solution.verticalOrder(
+                .init(
+                    3,
+                    .init(9),
+                    .init(20, .init(15), .init(7))
+                )
+            ),
+            [[9],[3,15],[20],[7]]
+        )
+        XCTAssertEqual(
+            solution.verticalOrder(
+                .init(
+                    3,
+                    .init(9, .init(4), .init(0)),
+                    .init(8, .init(1), .init(7))
+                )
+            ),
+            [[4],[9],[3,0,1],[8],[7]]
+        )
+        XCTAssertEqual(
+            solution.verticalOrder(
+                .init(
+                    3,
+                    .init(9, .init(4), .init(0, nil, .init(2))),
+                    .init(8, .init(1, .init(5), nil), .init(7))
+                )
+            ),
+            [[4],[9,5],[3,0,1],[8,2],[7]]
+        )
+    }
+}
+
