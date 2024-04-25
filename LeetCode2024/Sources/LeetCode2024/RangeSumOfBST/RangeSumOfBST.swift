b38c28fafb9828d08a0825c670a98a9dca67d489
Sergey Melnikov <smelnikov2@alfabank.ru>
2024-04-26 00:03:13 +0800

    938. Range Sum of BST - solution

--- LeetCode2024/Sources/LeetCode2024/RangeSumOfBST/RangeSumOfBST.swift
+++ LeetCode2024/Sources/LeetCode2024/RangeSumOfBST/RangeSumOfBST.swift
@@ -1,5 +1,22 @@
 final class RangeSumOfBST {
     func rangeSumBST(_ root: TreeNode?, _ low: Int, _ high: Int) -> Int {
-        0
+        var result = 0
+        parse(root, &result, low, high)
+        return result
+    }
+    
+    private func parse(_ root: TreeNode?, _ sum: inout Int, _ low: Int, _ hight: Int) {
+        guard let root else { return }
+        if root.val < low {
+            parse(root.right, &sum, low, hight)
+            return
+        }
+        if root.val > hight {
+            parse(root.left, &sum, low, hight)
+            return
+        }
+        sum += root.val
+        parse(root.right, &sum, low, hight)
+        parse(root.left, &sum, low, hight)
     }
 }
