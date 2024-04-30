762d8ca5a6c36107f3fbe9331ab8fa02a2c932c3
Sergey Melnikov <smelnikov2@alfabank.ru>
2024-04-30 14:23:39 +0800

    274. H-Index - solution

--- LeetCode2024/Sources/LeetCode2024/H-index/HIndex.swift
+++ LeetCode2024/Sources/LeetCode2024/H-index/HIndex.swift
@@ -1,5 +1,12 @@
 final class HIndex {
     func hIndex(_ citations: [Int]) -> Int {
-        0
+        let citations = citations.sorted(by: >)
+        var result = 0
+        for (index, value) in citations.enumerated() {
+            if value > index {
+                result += 1
+            } else { break }
+        }
+        return result
     }
 }
--- LeetCode2024/Tests/LeetCode2024Tests/HIndex/HIndexTests.swift
+++ LeetCode2024/Tests/LeetCode2024Tests/HIndex/HIndexTests.swift
@@ -10,12 +10,14 @@ final class HIndexTests: XCTestCase {
     
     func testHIndex() {
         XCTAssertEqual(solution.hIndex([3,0,6,1,5]), 3)
+        XCTAssertEqual(solution.hIndex([3,0,6,1]), 2)
         XCTAssertEqual(solution.hIndex([1,3,1]), 1)
         XCTAssertEqual(solution.hIndex([3,0,6,1,5]), 3)
         XCTAssertEqual(solution.hIndex([1]), 1)
         XCTAssertEqual(solution.hIndex([3]), 1)
         XCTAssertEqual(solution.hIndex([5,6]), 2)
         XCTAssertEqual(solution.hIndex([1,2,3,4,5]), 3)
+        XCTAssertEqual(solution.hIndex([0]), 0)
     }
 }
 
