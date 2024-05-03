90e847e0d9ad0790f9d794fba405754cdc8b24cb
Sergey Melnikov <smelnikov2@alfabank.ru>
2024-05-03 12:28:08 +0800

    53. Maximum Subarray - best solution

--- LeetCode2024/Sources/LeetCode2024/MaximumSubarray/MaximumSubarray.swift
+++ LeetCode2024/Sources/LeetCode2024/MaximumSubarray/MaximumSubarray.swift
@@ -1,18 +1,12 @@
 final class MaximumSubarray {
     func maxSubArray(_ nums: [Int]) -> Int {
-        let first = nums.first!
-        guard nums.count > 1 else { return first }
-        var currentSum = first
-        var currentMax = first
-        for i in 1..<nums.count {
-            let num = nums[i]
-            let newSum = currentSum + num
-            currentMax = max(currentMax, newSum, num)
-            if currentMax == num || newSum <= 0 {
-                currentSum = num
-            }
-            currentSum = max(newSum, num)
+        var result = nums[0]
+        var sum = 0
+        for num in nums {
+            sum += num
+            result = max(result, sum)
+            if sum < 0 { sum = 0 }
         }
-        return currentMax
+        return result
     }
 }
