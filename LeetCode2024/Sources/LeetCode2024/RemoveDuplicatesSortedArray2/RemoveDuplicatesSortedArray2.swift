a0c7cf87c163aba171de0d3a1e374dbb050a16c8
Sergey Melnikov <smelnikov2@alfabank.ru>
2024-05-06 13:57:38 +0800

    80. Remove Duplicates from Sorted Array II - solution

--- LeetCode2024/Sources/LeetCode2024/RemoveDuplicatesSortedArray2/RemoveDuplicatesSortedArray2.swift
+++ LeetCode2024/Sources/LeetCode2024/RemoveDuplicatesSortedArray2/RemoveDuplicatesSortedArray2.swift
@@ -1,14 +1,12 @@
 final class RemoveDuplicatesSortedArray2 {
     func removeDuplicates(_ nums: inout [Int]) -> Int {
-        var dic = [Int: Int]()
         var i = 0
-        while i < nums.count {
-            let num = nums[i]
-            dic[num, default: 0] += 1
-            let count = dic[num]!
-            guard count > 2 else { i += 1; continue }
-            nums.remove(at: i)
+        for num in nums {
+            if i < 2 || num != nums[i - 2] {
+                nums[i] = num
+                i += 1
+            }
         }
-        return nums.count
+        return i
     }
 }
