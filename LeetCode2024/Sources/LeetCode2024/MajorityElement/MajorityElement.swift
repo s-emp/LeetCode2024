70c168deb2911c7031dc888c52c00ad3da42bec0
Sergey Melnikov <smelnikov2@alfabank.ru>
2024-05-06 14:17:10 +0800

    169. Majority Element - two solution

--- LeetCode2024/Sources/LeetCode2024/MajorityElement/MajorityElement.swift
+++ LeetCode2024/Sources/LeetCode2024/MajorityElement/MajorityElement.swift
@@ -1,9 +1,11 @@
 final class MajorityElement {
     func majorityElement(_ nums: [Int]) -> Int {
         var dic = [Int: Int]()
+        let goal = nums.count / 2
         for num in nums {
             dic[num, default: 0] += 1
+            if dic[num]! > goal { return num }
         }
-        return dic.first { $0.value > nums.count / 2 }!.key
+        fatalError()
     }
 }
