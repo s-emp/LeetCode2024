a217af4c81db49bd1a4a0b1dc5c9bc43b519fe7e
Sergey Melnikov <smelnikov2@alfabank.ru>
2024-05-08 14:52:42 +0800

    Best Time to Buy and Sell Stock II - tests

---
+++ LeetCode2024/Sources/LeetCode2024/BestTimeBuySellStock2/BestTimeBuySellStock2.swift
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/?envType=study-plan-v2&envId=top-interview-150
+
+final class BestTimeBuySellStock2 {
+    func maxProfit(_ prices: [Int]) -> Int {
+        0
+    }
+}
---
+++ LeetCode2024/Tests/LeetCode2024Tests/BestTimeBuySellStock2/BestTimeBuySellStock2Tests.swift
@@ -0,0 +1,19 @@
+import XCTest
+@testable import LeetCode2024
+
+final class BestTimeBuySellStock2Tests: XCTestCase {
+    var solution: BestTimeBuySellStock2!
+    
+    override func setUp() {
+        solution = .init()
+    }
+    
+    func testMaxProfit() {
+        XCTAssertEqual(solution.maxProfit([7,1,5,3,6,4]), 7)
+        XCTAssertEqual(solution.maxProfit([3,10,1,4]), 10)
+        XCTAssertEqual(solution.maxProfit([1,2,3,4]), 3)
+        XCTAssertEqual(solution.maxProfit([0]), 0)
+        XCTAssertEqual(solution.maxProfit([4,3,2,1]), 0)
+    }
+}
+
