4202a43f0ff2af481ce4ae37de0d90e860764596
Sergey Melnikov <smelnikov2@alfabank.ru>
2024-05-08 20:51:44 +0800

    122. Best Time to Buy and Sell Stock II - other solution

--- LeetCode2024/Sources/LeetCode2024/BestTimeBuySellStock2/BestTimeBuySellStock2.swift
+++ LeetCode2024/Sources/LeetCode2024/BestTimeBuySellStock2/BestTimeBuySellStock2.swift
@@ -2,19 +2,10 @@
 
 final class BestTimeBuySellStock2 {
     func maxProfit(_ prices: [Int]) -> Int {
-        let result = prices.reduce((profit: 0, buy: prices[0], currentProfit: 0)) { last, price in
-            var currentProfit = last.currentProfit
-            var profit = last.profit
-            var buy = last.buy
-            if price < currentProfit + buy {
-                profit += currentProfit
-                currentProfit = 0
-                buy = price
-            } else {
-                currentProfit = price - buy
-            }
-            return (profit, buy, currentProfit)
+        var ans = 0
+        for i in 1..<prices.count where prices[i] > prices[i - 1] {
+            ans += prices[i] - prices[i - 1]
         }
-        return result.profit + result.currentProfit
+        return ans
     }
 }
