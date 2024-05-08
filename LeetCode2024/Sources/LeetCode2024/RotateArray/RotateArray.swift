fbeb381c62b66602a2f4c826a11d000864de6b33
Sergey Melnikov <smelnikov2@alfabank.ru>
2024-05-08 13:56:32 +0800

    189. Rotate Array - new solution

--- LeetCode2024/Sources/LeetCode2024/RotateArray/RotateArray.swift
+++ LeetCode2024/Sources/LeetCode2024/RotateArray/RotateArray.swift
@@ -2,11 +2,18 @@
 
 final class RotateArray {
     func rotate(_ nums: inout [Int], _ k: Int) {
-        let rotateCount = k % nums.count 
+        let rotateCount = k % nums.count
         guard rotateCount > 0 else { return }
-        nums.insert(contentsOf: nums.suffix(rotateCount), at: 0)
-        for _ in 0..<rotateCount {
-            nums.removeLast()
+        let suffix = nums.suffix(rotateCount)
+        var i = nums.count - 1
+        while i - rotateCount >= 0 {
+            nums.swapAt(i, i - rotateCount)
+            i -= 1
+        }
+        i = 0
+        for value in suffix {
+            nums[i] = value
+            i += 1
         }
     }
 }
