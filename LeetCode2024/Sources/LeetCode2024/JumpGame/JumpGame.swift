251911f8532696dd191c3aa8453f5a12742fb332
Sergey Melnikov <smelnikov2@alfabank.ru>
2024-05-09 14:19:44 +0800

    55. Jump Game - tests

---
+++ LeetCode2024/Sources/LeetCode2024/JumpGame/JumpGame.swift
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/jump-game/?envType=study-plan-v2&envId=top-interview-150
+
+final class JumpGame {
+    func canJump(_ nums: [Int]) -> Bool {
+        false
+    }
+}
---
+++ LeetCode2024/Tests/LeetCode2024Tests/JumpGame/JumpGameTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import LeetCode2024
+
+final class JumpGameTests: XCTestCase {
+    var solution: JumpGame!
+    
+    override func setUp() {
+        solution = .init()
+    }
+    
+    func testCanJump() {
+        XCTAssertEqual(solution.canJump([2,3,1,1,4]), true)
+        XCTAssertEqual(solution.canJump([3,2,1,0,4]), false)
+    }
+}
+
