c8ed86afb375764d677f45184db7b7294f7d61e2
Sergey Melnikov <smelnikov2@alfabank.ru>
2024-05-09 14:41:31 +0800

    55. Jump Game - bad solution

--- LeetCode2024/Sources/LeetCode2024/JumpGame/JumpGame.swift
+++ LeetCode2024/Sources/LeetCode2024/JumpGame/JumpGame.swift
@@ -2,6 +2,15 @@
 
 final class JumpGame {
     func canJump(_ nums: [Int]) -> Bool {
-        false
+        guard nums.count > 1 else { return true }
+        var nextSteps = Set<Int>([0])
+        let goal = nums.count - 1
+        for (index, num) in nums.enumerated() where nextSteps.contains(index) {
+            for i in 0+index...num+index {
+                nextSteps.insert(i)
+            }
+            if nextSteps.contains(goal) { return true }
+        }
+        return nextSteps.contains(goal)
     }
 }
