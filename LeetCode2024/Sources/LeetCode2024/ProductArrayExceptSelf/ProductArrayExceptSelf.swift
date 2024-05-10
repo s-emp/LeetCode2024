9926ba5ed834047c77775352ceb36b86004859aa
Sergey Melnikov <smelnikov2@alfabank.ru>
2024-05-10 14:10:59 +0800

    238. Product of Array Except Self - two bad solution

--- LeetCode2024/Sources/LeetCode2024/ProductArrayExceptSelf/ProductArrayExceptSelf.swift
+++ LeetCode2024/Sources/LeetCode2024/ProductArrayExceptSelf/ProductArrayExceptSelf.swift
@@ -3,13 +3,23 @@
 final class ProductArrayExceptSelf {
     func productExceptSelf(_ nums: [Int]) -> [Int] {
         var result = Array(repeating: 0, count: nums.count)
-        var currentResult = 1
+        let allResult = nums.reduce(1) { last, num in
+            return last * num
+        }
+        var zeroCount = 0
         for i in 0..<nums.count {
-            for j in 0..<nums.count where i != j {
-                currentResult *= nums[j]
+            if zeroCount == 2 { return Array(repeating: 0, count: nums.count) }
+            let num = nums[i]
+            if num == 0 {
+                zeroCount += 1
+                var currentResult = 1
+                for j in 0..<nums.count where i != j {
+                    currentResult *= nums[j]
+                }
+                result[i] = currentResult
+            } else {
+                result[i] = allResult / num
             }
-            result[i] = currentResult
-            currentResult = 1
         }
         return result
     }
