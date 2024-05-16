907dc3678130849f6d54856c252da4bce1820822
Sergey Melnikov <smelnikov2@alfabank.ru>
2024-05-16 12:59:54 +0800

    151. Reverse Words in a String - solution without split and joined

--- LeetCode2024/Sources/LeetCode2024/ReverseWordsInString/ReverseWordsInString.swift
+++ LeetCode2024/Sources/LeetCode2024/ReverseWordsInString/ReverseWordsInString.swift
@@ -2,6 +2,37 @@
 
 final class ReverseWordsInString {
     func reverseWords(_ s: String) -> String {
-        s.split(separator: " ").reversed().joined(separator: " ")
+        var startIndex = s.startIndex
+        var index = s.startIndex
+        var result: [String] = []
+        while index < s.endIndex {
+            if s[index] == " " {
+                guard startIndex != index else {
+                    startIndex = s.index(after: index)
+                    index = startIndex
+                    continue
+                }
+                let newString = String(s[startIndex..<index])
+                startIndex = s.index(after: index)
+                index = startIndex
+                if newString != " " {
+                    result.append(newString)
+                }
+            } else {
+                index = s.index(after: index)
+            }
+        }
+        if startIndex != index {
+            let newString = String(s[startIndex..<index])
+            if newString != " " {
+                result.append(newString)
+            }
+        }
+        var resultString = ""
+        for string in result.reversed() {
+            resultString.append(string + " ")
+        }
+        resultString.removeLast()
+        return resultString
     }
 }
--- LeetCode2024/Tests/LeetCode2024Tests/ReverseWordsInString/ReverseWordsInStringTests.swift
+++ LeetCode2024/Tests/LeetCode2024Tests/ReverseWordsInString/ReverseWordsInStringTests.swift
@@ -12,7 +12,7 @@ final class ReverseWordsInStringTests: XCTestCase {
         XCTAssertEqual(solution.reverseWords("Hello world"), "world Hello")
         XCTAssertEqual(solution.reverseWords("  Hello world  "), "world Hello")
         XCTAssertEqual(solution.reverseWords("  Hello  world  "), "world Hello")
-        XCTAssertEqual(solution.reverseWords("  hi gay and  world  "), "world and gay hi")
+        XCTAssertEqual(solution.reverseWords("a  hi gay and  world  "), "world and gay hi a")
     }
 }
 
