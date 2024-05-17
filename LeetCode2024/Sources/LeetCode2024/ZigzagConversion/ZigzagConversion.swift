1530d36b93a3104265f7af09efa14ac0e74668c8
Sergey Melnikov <smelnikov2@alfabank.ru>
2024-05-17 12:21:58 +0800

    6. Zigzag Conversion - test

---
+++ LeetCode2024/Sources/LeetCode2024/ZigzagConversion/ZigzagConversion.swift
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/zigzag-conversion/description/?envType=study-plan-v2&envId=top-interview-150
+
+final class ZigzagConversion {
+    func convert(_ s: String, _ numRows: Int) -> String {
+        var result = Array(repeating: String(), count: numRows)
+        var isLine = true
+        for (index, character) in s.enumerated() {
+            let 
+            if index % numRows == 0 {
+                isLine.toggle()
+                
+            }
+        }
+        return result.joined()
+    }
+}
---
+++ LeetCode2024/Tests/LeetCode2024Tests/ZigzagConversion/ZigzagConversionTests.swift
@@ -0,0 +1,22 @@
+import XCTest
+@testable import LeetCode2024
+
+final class ZigzagConversionTests: XCTestCase {
+    var solution: ZigzagConversion!
+    
+    override func setUp() {
+        solution = .init()
+    }
+    
+    func testConvert() {
+        var result = """
+        P     I    N
+        A   L S  I G
+        Y A   H R
+        P     I
+        """
+        XCTAssertEqual(solution.convert("PAYPALISHIRING", 4), result)
+        
+    }
+}
+
