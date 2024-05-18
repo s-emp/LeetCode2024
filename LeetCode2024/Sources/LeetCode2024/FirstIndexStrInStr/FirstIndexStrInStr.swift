bcd50d0286ee3bf7918ad601f724801e5c8ea0a7
Sergey Melnikov <smelnikov2@alfabank.ru>
2024-05-18 15:43:53 +0800

    28. Find the Index of the First Occurrence in a String - link

--- LeetCode2024/Sources/LeetCode2024/FirstIndexStrInStr/FirstIndexStrInStr.swift
+++ LeetCode2024/Sources/LeetCode2024/FirstIndexStrInStr/FirstIndexStrInStr.swift
@@ -1,4 +1,4 @@
-//
+// https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/?envType=study-plan-v2&envId=top-interview-150
 
 final class FirstIndexStrInStr {
     
