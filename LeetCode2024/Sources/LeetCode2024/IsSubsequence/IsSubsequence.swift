e95e1482e32262e9f8bf56bd6fa893c1df173be2
Sergey Melnikov <smelnikov2@alfabank.ru>
2024-05-21 12:10:23 +0800

    392. Is Subsequence - repetition

--- LeetCode2024/Sources/LeetCode2024/IsSubsequence/IsSubsequence.swift
+++ LeetCode2024/Sources/LeetCode2024/IsSubsequence/IsSubsequence.swift
@@ -2,17 +2,19 @@ final class IsSubsequence {
     func isSubsequence(_ s: String, _ t: String) -> Bool {
         guard s.count > 0 else { return true }
         guard t.count > 0 else { return false }
-        var sIterator = s.makeIterator()
-        var sCharater = sIterator.next()
-        var tIterator = t.makeIterator()
-        while let next = tIterator.next(), let findCharacter = sCharater {
-            if next == findCharacter {
-                guard let newFindCharacter = sIterator.next() else {
-                    return true
+        var index = t.startIndex
+        for c in s {
+            var isSearch = false
+            while index < t.endIndex {
+                if c == t[index] {
+                    index = t.index(after: index)
+                    isSearch = true
+                    break
                 }
-                sCharater = newFindCharacter
+                index = t.index(after: index)
             }
+            guard isSearch else { return false }
         }
-        return false
+        return true
     }
 }
