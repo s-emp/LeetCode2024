9e30671fabb1c9bdd72d508479ad15ea810784f3
Eeemp <mpt.s.a.melnikov@gmail.com>
2024-05-26 14:56:17 +0800

    3. Longest Substring Without Repeating Characters - repetation

--- LeetCode2024/Sources/LeetCode2024/LongestSubstring/LongestSubstring.swift
+++ LeetCode2024/Sources/LeetCode2024/LongestSubstring/LongestSubstring.swift
@@ -3,19 +3,22 @@
 final class LongestSubstring {
     func lengthOfLongestSubstring(_ s: String) -> Int {
         guard !s.isEmpty else { return 0 }
-        var last = s.startIndex
-        var len = 0
-        var characters = Set<Character>()
+        var dict: [Character: Int] = [:]
+        var lowerIndex = 0, currentIndex = 0, maxLenght = 0
         var iterator = s.makeIterator()
         while let next = iterator.next() {
-            if characters.insert(next).inserted == false {
-                len = max(len, characters.count)
-                repeat {
-                    characters.remove(s[last])
-                    last = s.index(after: last)
-                } while characters.insert(next).inserted == false
+            guard let chapterIndex = dict[next], lowerIndex <= chapterIndex else {
+                dict[next] = currentIndex
+                currentIndex += 1
+                continue
             }
+            let currentLength = currentIndex - lowerIndex
+            maxLenght = currentLength > maxLenght ? currentLength : maxLenght
+            dict[next] = currentIndex
+            lowerIndex = chapterIndex + 1
+            currentIndex += 1
         }
-        return max(len, characters.count)
+        let currentLength = currentIndex - lowerIndex
+        return currentLength > maxLenght ? currentLength : maxLenght
     }
 }
