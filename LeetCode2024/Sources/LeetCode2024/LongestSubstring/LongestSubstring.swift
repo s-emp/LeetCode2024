558feb200df5825122487c43c7c50f8e130bd102
Eeemp <mpt.s.a.melnikov@gmail.com>
2024-05-27 19:37:42 +0800

    LongestSubstring

--- LeetCode2024/Sources/LeetCode2024/LongestSubstring/LongestSubstring.swift
+++ LeetCode2024/Sources/LeetCode2024/LongestSubstring/LongestSubstring.swift
@@ -1,24 +1,26 @@
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/?envType=study-plan-v2&envId=top-interview-150
 
 final class LongestSubstring {
+    @_optimize(speed)
     func lengthOfLongestSubstring(_ s: String) -> Int {
+        // Мы идем окном, где lastIndex это левая граница, а currentIndex - правая граница. Когда доходим до найденного, ставим левую на следующий за найденным, потому что это и есть реальная новая левая граница
         guard !s.isEmpty else { return 0 }
-        var dict: [Character: Int] = [:]
-        var lowerIndex = 0, currentIndex = 0, maxLenght = 0
+        var lastIndex = 0, currentIndex = 0, maxLen = 0
         var iterator = s.makeIterator()
+        var dic = Dictionary<Character, Int>()
         while let next = iterator.next() {
-            guard let chapterIndex = dict[next], lowerIndex <= chapterIndex else {
-                dict[next] = currentIndex
+            guard let findIndex = dic[next], lastIndex <= findIndex else {
+                dic[next] = currentIndex
                 currentIndex += 1
                 continue
             }
-            let currentLength = currentIndex - lowerIndex
-            maxLenght = currentLength > maxLenght ? currentLength : maxLenght
-            dict[next] = currentIndex
-            lowerIndex = chapterIndex + 1
+            let currentLen = currentIndex - lastIndex
+            maxLen = max(maxLen, currentLen)
+            dic[next] = currentIndex
+            lastIndex = findIndex + 1
             currentIndex += 1
         }
-        let currentLength = currentIndex - lowerIndex
-        return currentLength > maxLenght ? currentLength : maxLenght
+        
+        return max(maxLen, currentIndex - lastIndex)
     }
 }
