fd8b16f071cfa19fa3a42700a5ea789442b6b3a2
Eeemp <mpt.s.a.melnikov@gmail.com>
2024-06-03 14:30:07 +0800

    1. Two Sum - repetition

--- LeetCode2024/Sources/LeetCode2024/TwoSum/TwoSum.swift
+++ LeetCode2024/Sources/LeetCode2024/TwoSum/TwoSum.swift
@@ -1,13 +1,15 @@
+// https://leetcode.com/problems/two-sum/submissions/1276025030/?envType=study-plan-v2&envId=top-interview-150
+
 class TwoSum {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        var dictionary = Dictionary<Int, Int>()
-        for i in 0..<nums.count {
-            let value = nums[i]
-            if let j = dictionary[target - value] {
-                return [j, i]
-            } else {
-                dictionary[value] = i
+        var dic = [Int: Int]()
+        for (index, num) in nums.enumerated() {
+            let searchNum = target - num
+            guard let previewIndex = dic[searchNum] else {
+                dic[num] = index
+                continue
             }
+            return [previewIndex, index]
         }
         return []
     }
