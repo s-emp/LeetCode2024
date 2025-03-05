b91d6eb7d3b38b329b82e2c301e6db194be08477
Emp <mpt.s.a.melnikov@gmail.com>
2025-03-05 17:43:08 +0300

    Two sum: новый старт в 2025

--- LeetCode2024/Sources/LeetCode2024/TwoSum/TwoSum.swift
+++ LeetCode2024/Sources/LeetCode2024/TwoSum/TwoSum.swift
@@ -1,15 +1,13 @@
 // https://leetcode.com/problems/two-sum/submissions/1276025030/?envType=study-plan-v2&envId=top-interview-150
-
+// 2025
 class TwoSum {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        var dic = [Int: Int]()
-        for (index, num) in nums.enumerated() {
-            let searchNum = target - num
-            guard let previewIndex = dic[searchNum] else {
-                dic[num] = index
-                continue
+        var dictionary = [Int: Int]()
+        for (index, value) in nums.enumerated() {
+            if let otherIndex = dictionary[target - value] {
+                return [otherIndex, index]
             }
-            return [previewIndex, index]
+            dictionary[value] = index
         }
         return []
     }
